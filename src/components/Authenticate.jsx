c13a9e199ad435ad38d0965f2f33889ce52c5f9b
ljadow <lindsay@jadow.com>
2023-12-13 15:19:12 -0500

    added input validation

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -8,7 +8,6 @@ export default function Authenticate({ token, setToken }) {
 
     async function handleClick(e) {
         e.preventDefault();
-
         try {
             const response = await fetch("https://fsa-jwt-practice.herokuapp.com/authenticate", {
                 method: "GET",
@@ -18,23 +17,23 @@ export default function Authenticate({ token, setToken }) {
                 }
             })
             const result = await response.json();
+            setToken(result.token)
             setsuccessMessage(result.message)
             setUserData(result.data.iat)
-            console.log(result.data)
-            console.log(result)
+            setError("")
         }
-        catch {
+        catch(error){
             setError(error.message)
+            console.log("error message: ",error.message)
         }
-
     }
 
     return (
         <div className="authenticate">
             <h2>Authenticate</h2>
-            {successMessage ? <p>{successMessage}</p> : ""}
+            {successMessage&&successMessage!=="jwt malformed" ? <p className="success">{successMessage}</p> : ""}
             {userData ? <p>User IAT: {userData}</p> : ""}
-            {error ? <p>{error}</p> : ""}
+            {error ? <p className="error">{error}</p> : ""}
             <button onClick={handleClick}>Authenticate Token</button>
         </div>
     )
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,28 +1,41 @@
 import { useState } from "react"
 import './formstyle.css'
 
-export default function SignUpForm({token,setToken}) {
+export default function SignUpForm({ token, setToken }) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState(null)
+    const [message, setMessage] = useState(null)
+
 
     async function handleSubmit(event) {
         event.preventDefault();
+        setError("")
+        setMessage("")
         try {
-            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
-                method: "POST",
-                headers: { "Content-Type": "application/json", },
-                body: JSON.stringify({
-                    username: { username },
-                    password: { password },
+            if (username.length < 7) {
+                return setError("Username must be 8+ characters, please try again")
+            }
+            else {
+                setError("")
+                setMessage("Submit Successful")
+                const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json", },
+                    body: JSON.stringify({
+                        username: { username },
+                        password: { password },
+                    })
                 })
-            })
-            const APIpost = await response.json();
-            setToken(APIpost.token)
-            console.log("API Post: ", APIpost)
-            console.log("Token: ", token)
-        }
-        catch {
+                const APIpost = await response.json();
+                setToken(APIpost.token)
+                // console.log("API Post: ", APIpost)
+                // console.log("Token: ", token)
+                setUsername("")
+                setPassword("")
+            }//else close
+        }//try close
+        catch(error) {
             setError(error.message)
             console.error(error)
         }
@@ -33,20 +46,22 @@ export default function SignUpForm({token,setToken}) {
     return (
         <div className="formContainer">
             <h2>Sign Up</h2>
-            {error ? <p>{error}</p> : ""}
+            {error ? <p className="error">{error}</p> : ""}
+            {message ? <p className="success">{message}</p> : ""}
             <form onSubmit={handleSubmit}>
                 <label> Username:
                     <input
                         value={username}
                         onChange={(event) => { setUsername(event.target.value) }}
                     />
-                </label><br/>
+                </label><br />
                 <label>Password:
                     <input
+                        type="password"
                         value={password}
                         onChange={(event) => { setPassword(event.target.value) }}
                     />
-                </label><br/><br/>
+                </label><br /><br />
                 <button type="submit">Submit</button>
             </form>
         </div>
